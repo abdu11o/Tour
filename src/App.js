185ed74075c4d989e98711871588d1ac16636380
nvhhbjbkj <abdullohismatullaev32@gmail.com>
2023-04-27 17:47:42 +0500

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import Navbar from './Components/Navbar'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-     <Navbar />
-    </div>
+    <div>Tour</div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <span className="logo">Samo</span>
+            </div>
+            <div className="prsentation"></div>
+        </header>
+        )
+}
